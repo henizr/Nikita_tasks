ba4dbc8dcdb9a6ec3152b2bb58a9016cf5808dba
palindrom8 <andr.nitgrafika@yandex.ru>
2024-07-31 13:49:32 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# импорты библиотек
+import main
+from graphlern import Pixel, Message
+
+
+# объявление переменных(констант)
+GRID_WIDTH = 10
+GRID_HEIGHT = 10
+PIXEL_SIZE = 50
+BACKGROUND_COLOR = "#ffeba1"
+SOUND = ""
+
+
+# отрисовка
+def draw(window):
+    
+    Pixel(window, x=5, y=1, color="brown", size=PIXEL_SIZE)
+    # Здесь пиши свой код
+ 
