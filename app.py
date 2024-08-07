6f509dbdbd9075bfe1880510bf727a235aac3a55
palindrom8 <andr.nitgrafika@yandex.ru>
2024-08-07 14:41:40 +0300

    Added task 3

--- app.py
+++ app.py
@@ -11,18 +11,15 @@ BACKGROUND_COLOR = "#ffeba1"
 SOUND = ""
 
 
-# Цвет смайла:  #030418
+ 
 
-# координаты x пикселей смайла
+# координаты x пикселей апельсина
 
-x_coords_when_y_equals_2 = []
-x_coords_when_y_equals_3 = []
-x_coords_when_y_equals_7 = []
-x_coords_when_y_equals_8 = []
-
+x_coords_when_y_equals_1 = []
+ 
 
 # отрисовка
 def draw(window):
     # Здесь пиши свой код
     
-    Pixel(window, x=1, y=2, color="#030418", size=PIXEL_SIZE)
+    Pixel(window, x=1, y=2, color="orange", size=PIXEL_SIZE)
